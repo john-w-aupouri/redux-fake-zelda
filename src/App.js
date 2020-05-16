3182dd1e5069b88c4ddf1a8be321064921213047
john_wirihana_aupouri <johnaupouri963@gmail.com>
2020-05-16 16:48:22 +1200

    changed map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './logo.svg';
 import logo1 from './redux.svg';
 
 import World from './features/world';
@@ -10,13 +9,9 @@ class App extends Component {
   render() { 
     return (
       <div className="App">
-        <section>
-          <img id="redux-logo" src={logo1} alt="redux-logo" />
-          <div>
-            <World />
-          </div>
-          <img id="react-logo" src={logo} alt="react-logo" />
-        </section>
+        <div className="world-wrapper">
+          <World />
+        </div>
       </div>
     );
   }
